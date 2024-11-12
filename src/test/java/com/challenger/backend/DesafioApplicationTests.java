8188ca663bb056b96397eea68dfa567e48b7785c
Kayo Leanndro <kayoleanndro2@gmail.com>
2024-11-12 18:22:23 -0300

    organizando projeto

---
+++ src/test/java/com/challenger/backend/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenger.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
